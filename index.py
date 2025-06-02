0b9d3472284b2e6b7accc0f293da2e7930625093
Victoria-Ogega <victoriaogega@gmail.com>
2025-06-02 21:30:41 +0300

    Refactored the Account class

--- index.py
+++ index.py
@@ -1,97 +1,160 @@
-from datetime import datetime, timedelta
-from statistics import mean
+import datetime
+
+class Transaction:
+    def __init__(self,date,narration,transaction_type,amount):
+        self.date= date
+        self.narration = narration
+        self.transaction_type = transaction_type
+        self.amount = amount
+
+    def get_details (self):
+        return {
+            'date':self.date,
+            'narration':self.narration,
+            'transaction_type':self.transaction_type,
+            'amount':self.amount
+        }   
 
 class Account:
-    def __init__(self,name,balance = 0.0,limit= 50000):
+    def __init__(self,name,account_no):
         self.name = name
-        self.deposit = []
-        self.withdrawals = []
-        self.balance = balance
-        self.loan_intrest_rate = 0.05
-        self.loans = loans
-        self.transfers = transfers
+        self._account_no = account_no
+        self.transaction = []
+        self._balance = 0
+        self.loan_balance = 0
+        self.loan = 0
+        self.new_loan_amount=0
+
+    def deposit(self,amount,narration ='deposit'):
+        if self.is_frozen:
+            print("Account is frozen,you cannot deposit") 
+        if amount < 0:
+            print ("Input a positive number")
+        else:
+            current_date = datetime.datetime.now()
+            new_transaction = Transaction(current_date,narration,'deposit',amount)
+            self.transaction.append(new_transaction)
+            self._balance += amount
+            print(f"Deposited {amount} to {self.name} at {new_transaction.date} account and your balance is {self.balance}")                  
+
+    def withdraw(self,amount,narration='withdraw'):
+        if self.is_frozen:
+            print("Account is frozen,you cannot withdraw") 
+        if amount < 0:
+            print("Input a positive number")
+        if amount > self.balance:
+            print ("Insufficient balance")
+        else:
+            current_date = datetime.datetime.now()
+            new_transaction = Transaction(current_date,amount,narration,'withdraw')
+            self.transaction.append(new_transaction)  
+            self._balance -=amount
+            print(f"Dear {self.name} you have withdrawed {amount} and your balance is {self.balance} at {new_transaction.date}")    
+
+    def transfering(self,amount,recepient,narration = 'narration'):
+        if self.is_frozen:
+            print("Account is frozen, you cantinue with your transaction.") 
+        if amount < 0: 
+            print("Please input a positive value")
+        if amount > self._balance:
+            print("Insufficient funds") 
+        else:
+            current_date = datetime.datetime.now()
+            new_transaction = Transaction(current_date,amount,narration,'transfering')
+            self.transaction.append(new_transaction)
+            self._balance -=amount
+            print(f"you have transfered {amount} to {recepient} and your balance is {self.balance}")    
+
+
+    def  get_balance(self):
+        remaining balance = sum(transaction.get_details()['amount']for transaction in self.transaction )
+        balance = sum(transaction.get_details()['amount']) for transaction in self.transaction
+        self._balance = balance
+        return self.get_balance
+      
+   
+
+    def get_total_deposits(self):
+        total_amount = sum(
+            transaction.get_details()['amount']
+            for transaction in self.transaction
+            if transaction.get_details()['transaction_type'] in ['deposit','Initial deposit']
+            and transaction.get_details()['amount']
+        )
+        return total_amount                          
+
+
+    def get_loan(self,amount,narration = 'loan'): 
+            if self.is_frozen:
+                print("Account is frozen") 
+            if amount <0 :
+                print("Enter a positive value")
+            if self.loan_balance >0:
+                return "You have an outstanding debt." 
+            if amount > self.get_total_deposits():
+                print("The amount is past your loan limit,please try a lower amount")    
+            else:
+                current_date = datetime.datetime.now()
+                new_transaction = Transaction(current_date,amount,narration,'loan')
+                self.loan_balance += amount
+                self.balance +=amount
+                self.transaction.append(new_transaction)
+                self.loan = amount
+                return f"You have received a loan of {amount} at {new_transaction.date} and your new balance is {self.balance}. Your loan balance is {self.loan}"       
+
+    def interest_rate(self,rate,time_in_years):
+        loan_amount = self.loan *rate*time_in_years
+        self.new_loan_amount += self.loan_balance + loan_amount
+        return self.new_loan_amount
        
-
-    def deposits(self,amount):
-        if amount > 0:
-            self.transactions.append(amount)
-            self.balance += amount
-            return f"You have deposited {amount} to your account successfully and your balance is {self.show_balance()}"
-            
-
-    def show_balance(self):
-        return f"Your balance is {self.balance} shillings."
- 
-
-    def transfer(self,amount):
-        if amount > self.balance:
-            print("Insufficient balance. Your current balance is {self.balance} KSH.")
+             
+    def repay_loan(self,amount,narration = 'repay_loan'):
+        if amount <0 :
+            print("Enter a positive value")
+        if amount <= self.loan_balance:
+            current_date = datetime.datetime.now()
+            new_transaction = Transaction(current_date,amount,narration,'repay_loan')
+            self.loan_balance = self.new_loan_amount -amount
+            return f"You have repayed your loan of {self.loan} with {amount} at {new_transaction.date} and your balance is {self.loan_balance}"   
         else:
-            self.transactions.append(amount) 
-            self.balance -= amount 
-            return f"You have successfully transfered {amount} shillings and your balance is {self.balance} KSH"               
+            current_date = datetime.datetime.now()
+            new_transaction = Transaction(current_date,amount,narration,'repay_loan')
+            extra_amount = amount - self.loan_balance
+            self._balance += extra_amount
+            return f"You have repayed your loan of {self.loan} with {amount} at {new_transaction.date} and your balance is {self.loan_balance}"  
 
-    def withdraw(self,amount):
-        if amount > self.balance:
-            print("Insufficient balance.Your current balance is {self.balance}KSH.")
-        elif amount <= 0:
-            print("Withdrawal amount must be positive.")  
-        else:
-            self.transactions.append(amount)
-            self.balance -= amount
-            return f"You have successfully withdrawed {amount} shillings from your account and your balance is {self.balance} KSH."
-                
-     def get_loan(self, amount):
-        if amount > 0:
-            pass
-        else:
-            return "You can't take a negative loan."
-        if amount > self.limit:
-            return "Please try a smaller amount."  
-        else:
-            self.loans.append(amount)
-            self.balance += amount
+    def freeze_account(self): 
+        self_.is_frozen = true
+        return "Account has been frozen"  
 
-                  
+    def unfreeze_account(self): 
+        self_.is_frozen = false
+        return "Account has been unfrozen" 
 
-     def repay_loan(self, amount):
-        if amount <=0:
-            return "Loan repayment failed, amount has to be positive."
-        total_outstanding_debt = sum(loans["amount"]-loans["repaid_amount"]for loans in self.loans if not loan.get ("Fully re-paid", false))
-        if total_outstanding_debt == 0:
-            return "No outstanding loans to repay"   
-        if amount >  total_outstanding_debt:
-                return f"Repayment amount {amount} esxceeds total outstanding loan debt of {total_outstanding_debt}"
-                amount_to_repay = total_outstanding_debt
-        else: amount_to_repay = amount
+    def close_account(self,account):
+        deposits = [
+            transaction for transaction in self.transaction
+            if transaction.get_details()['transaction_type']
+        ]
+        if not deposits:
+            last_deposit = max(transaction.get_details()['date'] for transaction in deposits )
+            if last_deposit < datetime.now() - timedelta(days = 90):
+                account.close_account()
+                return "Account closed"
 
-        if self.balance < amount_to_repay:
-                 print(f"Loan repayment failed: Insufficient funds in account to repay ${amount_to_repay}. Current balance: ${self.balance}")
-            return
+    def account_statement(self):
+        if not self.transactions:
+            return "No transactions found."
+            statement = f"Account Statement for {self.name}"
+        for transaction in self.transactions:
+            statement += f"Date: {transaction.date}, Narration: {transaction.narration}, Type: {transaction.transaction_type}, Amount: {transaction.amount}"
+            return statement            
 
 
-                 
-
-     def get_statement(self):
-        statement = f"Account statement for {self.name} Deposit: {self.depositt}withdrawals: {self.withdrawals}transfers: {self.transfers} Balance: {self.get_balance()}"
-        return statement
-
-     def get_loan_statement(self):
-        statement = f"Loan statement for {self.name} Loans taken: {self.loans} Loan repayments: {self.loan_repaying}Unpaid loan: {sum(self.loans) - sum(self.loan_repaying)}"
-        return statement
-
-     def freeze_account(self):
-        self.freeze=true
-        return "Account frozen"
-
-      def unfreeze_account(self):
-        self.freeze=false
-        return "Account unfrozen"
-
-     def view_account_details(self):
-         return (f"{self.name}", your account balance is {self.balance()})        
 
 
-     def change_owner(self,new_owner):
-        self.new_owner=new_owner
-        return f"New account holder is {new_owner}"
+
+
+                            
+
+                        
